e97c7f058b40b5b8e8d488087f2531aa5441e284
Julia Sun <julia.sun@berkeley.edu>
2016-01-20 15:18:53 -0800

    renamed some things

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ For InnoD Marketing/Recruitment Spring 2016
 ## TODO
 * Animate pages (fadein)
 * Animate lists
-* Think about how to split up 'career'
 * Fix link preview
 * Remove URL changes in final iteration
 
---
+++ src/js/controllers/greekCtrl.js
@@ -0,0 +1,56 @@
+app.controller('greekCtrl', ['$scope', function($scope) {
+    $scope.frats = [
+        {
+            name: 'Alpha Chi Sigma (Chemistry)',
+            link: 'http://axs.berkeley.edu/rush/'
+        },
+        {
+            name: 'Alpha Epsilon Zeta (Multi-Discplinary)',
+            link: 'http://aez.berkeley.edu/mission/'
+        },
+        {
+            name: 'Alpha Kappa Psi (Business)',
+            link: 'http://www.calakpsi.com/'
+        },
+        {
+            name: 'Alpha Phi Omega (Service)',
+            link: 'http://live.calaphio.com/rush/index.html'
+        },
+        {
+            name: 'Delta Sigma Pi (Business)',
+            link: 'http://www.dsp-rho.com/'
+        },
+        {
+            name: 'Eta Omega Chi (Entrepreneurship)',
+            link: 'http://www.calhox.org/'
+        },
+        {
+            name: 'Kappa Alpha Pi (Pre-Law)',
+            link: 'http://www.kapiucb.com/#!about/cjg9'
+        },
+        {
+            name: 'Phi Alpha Delta (Pre-Law)',
+            link: 'http://www.berkeleypad.org/'
+        },
+        {
+            name: 'Phi Chi (Pre-Med)',
+            link: 'http://phichi.berkeley.edu/AboutUs.htm'
+        },
+        {
+            name: 'Pi Sigma Epsilon (Sales, Marketing, Management)',
+            link: 'http://berkeleypse.org/'
+        },
+        {
+            name: 'Sigma Eta Pi (Entrepreneurship)',
+            link: 'http://www.ucberkeleysep.com/'
+        },
+        {
+            name: 'Sigma Mu Delta (Pre-Med)',
+            link: 'http://www.smdberkeley.com/'
+        },
+        {
+            name: 'Theta Tau (Engineering)',
+            link: 'http://www.calthetatau.com/'
+        }
+    ];
+}]);
